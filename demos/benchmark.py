be326e1446dea2c8656b6bf5bfc24e7bf8c0a476
Dmitry Marakasov <amdmi3@amdmi3.ru>
2018-02-08 18:11:27 +0300

    Add demo scripts

---
+++ demos/benchmark.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# Copyright (c) 2018 Dmitry Marakasov <amdmi3@amdmi3.ru>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from timeit import timeit
+
+
+def benchmark(name, stmt, setup, number=100000):
+    time = timeit(stmt=stmt, setup=setup, number=number)
+
+    print('{:>20}: {:.2f} M/sec'.format(name, number / time / 1000000.0))
+
+
+# number of iterations is tweaked so all tests take approx the same time
+benchmark('version_compare', 'version_compare("1.2.3", "2.3.4")', 'from competitors import version_compare', 3400000)
+benchmark('Version class', 'Version("1.2.3") < Version("2.3.4")', 'from competitors import Version', 220000)
+benchmark('versiontuple', 'versiontuple("1.2.3") < versiontuple("2.3.4")', 'from competitors import versiontuple', 190000)
+benchmark('StrictVersion', 'StrictVersion("1.2.3") < StrictVersion("2.3.4")', 'from competitors import StrictVersion', 70000)
+benchmark('LooseVersion', 'LooseVersion("1.2.3") < LooseVersion("2.3.4")', 'from competitors import LooseVersion', 50000)
+benchmark('parse_version', 'parse_version("1.2.3") < parse_version("2.3.4")', 'from competitors import parse_version', 20000)
---
+++ demos/compare.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# Copyright (c) 2018 Dmitry Marakasov <amdmi3@amdmi3.ru>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from competitors import competitors
+
+from tabulate import tabulate
+
+
+def get_cmp_sign(a, b):
+    """Convert comparison result to single character representation."""
+    if a < b:
+        return '<'
+    elif a > b:
+        return '>'
+    return '='
+
+
+def format_test_outcome(value):
+    if value is None:
+        return 'fail'
+    elif value:
+        return '+'
+    return '-'
+
+
+def evaluate_single_case(test_case, competitors):
+    left, expected_result, right = test_case.split(' ')
+
+    for _, wrapper in competitors:
+        try:
+            yield get_cmp_sign(wrapper(left), wrapper(right)) == expected_result
+        except:
+            yield(None)
+
+
+def evaluate_all_cases(test_cases, competitors):
+    return [
+        [test_case] + list(map(format_test_outcome, evaluate_single_case(test_case, competitors)))
+        for test_case in test_cases
+    ]
+
+
+test_cases = [
+    '1.0 = 1.0',
+    '1.0 = 1.00',
+    '1.0 = 01.0',
+    '1.0 = 1.0.0',
+    '1.2.3 = 1.2-3',
+    '1.0alpha1 = 1.0.alpha1',
+    '0.9 < 1.0',
+    '0.9 < 1.0alpha1',
+    '1.0alpha1 < 1.0alpha2',
+    '1.0alpha2 < 1.0beta1',
+    '1.0beta1 < 1.0pre1',
+    '1.0pre1 < 1.0rc1',
+    '1.0rc1 < 1.0',
+    '1.0 < 1.0patch1',
+    '1.0 < 1.0pl1',
+    '1.0 < 1.0post1',
+    '1.0.2 < 1.0.2a',
+    '1.0.2a < 1.0.2g',
+]
+
+
+print(
+    tabulate(
+        evaluate_all_cases(test_cases, competitors),
+        headers=['Test case'] + [competitor[0] for competitor in competitors]
+    )
+)
